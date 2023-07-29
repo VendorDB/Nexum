eda96652f1b8debc15ce025d70fea6e063f9d8be
xenorio <dev@xenorio.xyz>
2023-07-29 11:45:16 +0200

    logout

---
+++ src/routes/v1/user/logout.ts
@@ -0,0 +1,33 @@
+// Copyright (C) 2023 Marcus Huber (xenorio) <dev@xenorio.xyz>
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import { Handler } from 'express'
+
+export const post: Handler = async (req, res) => {
+
+	if (req.bot) {
+		res.status(403).json({
+			status: 'ERROR',
+			error: 'USER_ONLY',
+			message: 'This endpoint is only available to users'
+		})
+		return
+	}
+
+	res.clearCookie('session').json({
+		status: 'SUCCESS'
+	})
+
+}
